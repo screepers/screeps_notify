98cc8217e862dc17379bf49c0da105f9271dacce
Robert Hafner <tedivm@tedivm.com>
2016-06-12 21:19:21 -0700

    Added bin wrappers to activate virtualenvs when running programs

---
+++ bin/screepsnotifyctl.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Ths wrapper script activats the virtual environment before calling the
+# statsrunner.py program. It also makes sure the application is not run as
+# the root user.
+
+USER='screepsnotify'
+
+# Get real directory in case of symlink
+if [[ -L "${BASH_SOURCE[0]}" ]]
+then
+  DIR="$( cd "$( dirname $( readlink "${BASH_SOURCE[0]}" ) )" && pwd )"
+else
+  DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+fi
+cd $DIR
+
+ENV="$DIR/../env/bin/activate"
+if [ ! -f $ENV ]; then
+    echo 'Virtual Environment Not Installed'
+    exit -1
+fi
+
+SCRIPT="$DIR/../screeps_notify/notifyctl.py"
+
+if (( "$EUID" == 0 )); then
+  su - $USER -s /bin/bash -c "source $ENV; $SCRIPT $@"
+else
+  source $ENV
+  $SCRIPT "$@"
+fi
